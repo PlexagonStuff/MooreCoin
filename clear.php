622e7da18671b54997d0a51e9cf4ae79d430144c
Nicholas-Stokes <58690071+Nicholas-Stokes@users.noreply.github.com>
2022-05-24 16:20:48 -0400

    Add files via upload

--- clear.php
+++ clear.php
@@ -6,13 +6,25 @@ session_start();
 <head> <title> Updating Balances </title></head>
 <body>
 <?php
-//Storing 
-$clear = fopen("backend.json", 'r');
-fclose($clear);
-unlink("backend.json");
+//This opens the file for reading only, then closes it so that it can be deleted by the unlink function
 $clear = fopen("wallet.json", 'r');
 fclose($clear);
+unlink("wallet.json");
+$clear = fopen("login.json", 'r');
+fclose($clear);
 unlink("login.json");
+
+//This block recreates the login and wallet files and repopulates it with admin information
+$id = "admin";
+$array = array($id => array("password" => 1234));
+$json = json_encode($array);
+file_put_contents("login.json", $json);
+
+$id = "admin";
+$array = array($id => array("coins" => 0));
+$json = json_encode($array);
+file_put_contents("wallet.json", $json);
+
 header('Location: wallet.php');
 exit;
   ?>
